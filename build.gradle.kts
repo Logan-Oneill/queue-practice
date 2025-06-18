fac7db0e7d1b751993f9881677e7ae240716ea37
Logan Miller <13913229+Logan-Oneill@users.noreply.github.com>
2025-06-17 20:06:56 -0700

    working on docker file for running rabbitmq

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
+import com.bmuschko.gradle.docker.tasks.container.DockerCreateContainer
+import com.bmuschko.gradle.docker.tasks.container.DockerStartContainer
+import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
+
 plugins {
     kotlin("jvm")
     id("org.springframework.boot") apply false
     id("io.spring.dependency-management")
     kotlin("plugin.spring")
     java
+//    plugin dsl
+//        id("com.bmuschko.docker-remote-api") version "9.4.0"
 }
 
 group = "com.queueservice"
@@ -17,3 +23,49 @@ dependencyManagement {
         mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
     }
 }
+
+// support for running docker files with gradle tasks
+// following https://bmuschko.github.io/gradle-docker-plugin/current/user-guide/
+buildscript {
+    repositories {
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("com.bmuschko:gradle-docker-plugin:9.4.0")
+    }
+}
+
+apply(plugin = "com.bmuschko.docker-remote-api")
+
+// Use task types
+//tasks.create("buildRabbitMqContainer", DockerBuildImage::class) {
+//    inputDir.set(file("Dockerfile"))
+//    images.add("rabbitMqLocal:latest")
+//}
+
+val buildRabbitMqContainer by tasks.creating(DockerBuildImage::class) {
+    inputDir.set(file("Dockerfile"))
+    images.add("test/myapp:latest")
+}
+
+val createMyAppContainer by tasks.creating(DockerCreateContainer::class) {
+    dependsOn(buildRabbitMqContainer)
+    targetImageId(buildRabbitMqContainer.getImageId())
+    hostConfig.portBindings.set(listOf("8080:8080"))
+    hostConfig.autoRemove.set(true)
+}
+
+val startMyAppContainer by tasks.creating(DockerStartContainer::class) {
+    dependsOn(createMyAppContainer)
+    targetContainerId(createMyAppContainer.getContainerId())
+}
+
+val stopMyAppContainer by tasks.creating(DockerStopContainer::class) {
+    targetContainerId(createMyAppContainer.getContainerId())
+}
+
+tasks.create("functionalTestMyApp", Test::class) {
+    dependsOn(startMyAppContainer)
+    finalizedBy(stopMyAppContainer)
+}
+
--- messageSender/src/main/kotlin/com/queueservice/messagesender/routes/MessageController.kt
+++ messageSender/src/main/kotlin/com/queueservice/messagesender/routes/MessageController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/enqueueMessage")
-class messageController(
+class MessageController(
     val messageService: MessageService
 ) {
 
