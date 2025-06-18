f9a375e9230a539f1f0ec8cfdaa9d465e70e5740
logan-oneill <13913229+Logan-Oneill@users.noreply.github.com>
2025-06-17 20:30:04 -0700

    added docker compose for starting rabbitmq management server

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import com.bmuschko.gradle.docker.tasks.container.DockerCreateContainer
-import com.bmuschko.gradle.docker.tasks.container.DockerStartContainer
-import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
 
 plugins {
     kotlin("jvm")
@@ -8,8 +5,7 @@ plugins {
     id("io.spring.dependency-management")
     kotlin("plugin.spring")
     java
-//    plugin dsl
-//        id("com.bmuschko.docker-remote-api") version "9.4.0"
+    id("com.avast.gradle.docker-compose") version "0.17.12"
 }
 
 group = "com.queueservice"
@@ -24,48 +20,6 @@ dependencyManagement {
     }
 }
 
-// support for running docker files with gradle tasks
-// following https://bmuschko.github.io/gradle-docker-plugin/current/user-guide/
-buildscript {
-    repositories {
-        gradlePluginPortal()
-    }
-    dependencies {
-        classpath("com.bmuschko:gradle-docker-plugin:9.4.0")
-    }
+dockerCompose {
+    useComposeFiles = listOf("docker-compose.yml")
 }
-
-apply(plugin = "com.bmuschko.docker-remote-api")
-
-// Use task types
-//tasks.create("buildRabbitMqContainer", DockerBuildImage::class) {
-//    inputDir.set(file("Dockerfile"))
-//    images.add("rabbitMqLocal:latest")
-//}
-
-val buildRabbitMqContainer by tasks.creating(DockerBuildImage::class) {
-    inputDir.set(file("Dockerfile"))
-    images.add("test/myapp:latest")
-}
-
-val createMyAppContainer by tasks.creating(DockerCreateContainer::class) {
-    dependsOn(buildRabbitMqContainer)
-    targetImageId(buildRabbitMqContainer.getImageId())
-    hostConfig.portBindings.set(listOf("8080:8080"))
-    hostConfig.autoRemove.set(true)
-}
-
-val startMyAppContainer by tasks.creating(DockerStartContainer::class) {
-    dependsOn(createMyAppContainer)
-    targetContainerId(createMyAppContainer.getContainerId())
-}
-
-val stopMyAppContainer by tasks.creating(DockerStopContainer::class) {
-    targetContainerId(createMyAppContainer.getContainerId())
-}
-
-tasks.create("functionalTestMyApp", Test::class) {
-    dependsOn(startMyAppContainer)
-    finalizedBy(stopMyAppContainer)
-}
-
